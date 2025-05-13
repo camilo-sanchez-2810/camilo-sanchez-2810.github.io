018f0d894699e26818f2b383078254de3eacb73f
Camilo Sanchez <camsanbar@gmail.com>
2025-05-13 13:32:36 -0400

    change: cambio colores ui

--- index.js
+++ index.js
@@ -28,10 +28,12 @@
 
 	const projects = [
 		{
-			title: "Cashap - Gestor Financiero",
+			title: "Buddap - Gestor Financiero",
 			type: "FullStack",
 			categories: ["Financiero"],
 			status: "En progreso",
+			description:
+				"Plataforma web de finanzas personales que permite a los usuarios visualizar, planificar y optimizar sus gastos mensuales. Incorpora inteligencia artificial para ofrecer recomendaciones personalizadas y detectar patrones de consumo, facilitando la toma de decisiones financieras.",
 			image:
 				"https://dummyimage.com/600x400/000/fff&text=Buddap",
 			technologies: [
@@ -62,6 +64,8 @@
 			type: "FullStack",
 			categories: ["Financiero"],
 			status: "Terminado",
+			description:
+				"Sistema integral de gestión financiera orientado a la administración de múltiples sucursales en un asilo de adultos mayores. Permite el seguimiento detallado de gastos por sede, brindando reportes consolidados para una mejor toma de decisiones administrativas.",
 			image: "/images/software_rrhh.jpeg",
 			technologies: [
 				{
@@ -87,6 +91,8 @@
 			type: "Backend",
 			categories: ["Tecnología"],
 			status: "Demo",
+			description:
+				"Asistente virtual basado en inteligencia artificial que interactúa vía WhatsApp para brindar información en tiempo real sobre los locales de un centro comercial. Desarrollado con tecnologías modernas, permite mejorar la atención al cliente mediante respuestas automatizadas e inteligentes.",
 			image:
 				"https://dummyimage.com/600x400/000/fff&text=Assist+Bot",
 			technologies: [
@@ -105,6 +111,8 @@
 			type: "Backend / FullStack",
 			categories: ["Seguridad"],
 			status: "Terminado",
+			description:
+				"Plataforma de seguridad comunitaria que permite monitorear en tiempo real las rutas de patrullaje, generar reportes detallados y gestionar turnos. Diseñado para mejorar la coordinación vecinal y reforzar la seguridad en barrios o comunas mediante el uso de tecnología de seguimiento y análisis.",
 			image: "/images/comunidad_alerta.jpg",
 			technologies: [
 				{
@@ -138,6 +146,8 @@
 			type: "Backend",
 			categories: ["E-commerce"],
 			status: "Terminado",
+			description:
+				"Backend para una tienda online especializada en vinilos, que permite gestionar productos, usuarios y pedidos de forma eficiente. Optimizado para integrarse con interfaces de e-commerce, proporcionando una experiencia de compra ágil y segura.",
 			image: "/images/vinland.ico",
 			technologies: [
 				{
@@ -195,14 +205,12 @@
     	</div>
       <div class="project-card__content">
         <h3 class="project-card__title">Gestor Financiero</h3>
-        <p class="project-card__type">
-          <strong>Tipo:</strong> Fullstack
-        </p>
-        <p class="project-card__category">
-          <strong>Categoría:</strong> Financiero
+        <p class="project-card__description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia quos sequi facere saepe, non labore
+        	aut voluptate nulla ipsa, modi, reprehenderit quia vel molestias deleniti blanditiis sed ab corrupti
+        	reiciendis!
         </p>
         <div class="project-card__technologies">
-          <strong>Tecnologías:</strong>
           <ul class="tech-list">
             <li><i class="fab fa-js"></i></li>
             <li><i class="fab fa-react"></i></li>
@@ -210,7 +218,7 @@
           </ul>
         </div>
       </div>
-    </div>
+    </div><i class="${technology.icon}"></i>
 	*/
 
 	const projectsContent = select(
@@ -221,42 +229,36 @@
 		projectsContent.innerHTML = projects
 			.map((project) => {
 				return `
-				<div class="project-card">
-					<div class="project-card__header">
-						<img src="${
+	 			<div class="project-card">
+	 				<div class="project-card__header">
+	 					<img src="${
 							project.image
 						}" alt="Project 1 Image" class="project-card__image" />
-						<div class="project-card__status">
-							<span class="status-label">${
+	 					<div class="project-card__status">
+	 						<span class="status-label">${
 								project.status
 							}</span>
-						</div>
-					</div>
-					<div class="project-card__content">
-						<h3 class="project-card__title">${
+	 					</div>
+	 				</div>
+	 				<div class="project-card__content">
+	 					<h3 class="project-card__title">${
 							project.title
 						}</h3>
-						<p class="project-card__type">
-							<strong>Tipo:</strong> ${project.type}
+	 					<p class="project-card__description">
+						 	${project.description}
 						</p>
-						<p class="project-card__category">
-							<strong>Categoría:</strong> ${project.categories.join(
-								", "
-							)}
-						</p>
-						<div class="project-card__technologies">
-							<strong>Tecnologías:</strong>
-							<ul class="tech-list">
-								${project.technologies
+	 					<div class="project-card__technologies">
+	 						<ul class="tech-list">
+	 							${project.technologies
 									.map((technology) => {
-										return `<li>${technology.name} <i class="${technology.icon}"></i></li>`;
+										return `<li>${technology.name}</li>`;
 									})
 									.join("")}
-							</ul>
-						</div>
-					</div>
-				</div>
-			`;
+	 						</ul>
+	 					</div>
+	 				</div>
+	 			</div>
+	 		`;
 			})
 			.join("");
 	};
